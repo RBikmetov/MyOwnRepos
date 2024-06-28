e704237db9d80bd3926f6ba56944b1d12bdb136a
NightCrow <orahome@mail.ru>
2024-06-28 11:00:03 +0500

    Try to work with github

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,7 +11,7 @@ JPanel panel1 = new JPanel();
 JLabel label1 = new JLabel("Hell in the World");
 panel1.add(label1);
 this.add(panel1);
-this.setTitle("Привет, мир");
+this.setTitle("What's going on?");
 this.setSize(500,500);
 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 this.setVisible(true);
